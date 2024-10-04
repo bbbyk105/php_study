8697228a48921fd29d8c81cbd4498a2d693b6454
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-04 19:48:17 +0900

    配列の考え方

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
 
-// constant 定数
+$array_1 =["ああああ",2,3];
 
-const Max = 'テスト';
+$array_2=[
+    ["あか","青", "黄色"],
+    ["白","紫","黒"]
+];
+// echo $array[0];
 
-echo Max;
+echo ('<pre>');
+var_dump($array_2);
+echo ('<pre>');
 
+echo $array_2[1][1];
 
 ?>
