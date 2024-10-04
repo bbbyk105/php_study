5b8f98f2981960046ece0be617e58d15327db14d
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-04 19:31:34 +0900

    変数の使い方

--- val.php
+++ val.php
@@ -1,10 +1,14 @@
 <?php
 
-$test = 123;
-$test = "テストです";
+// 定数名先頭は英文字orアンダーバー
+$test_1 = 123;
+$test_2= 456;
+
+$test_3 = $test_1 . $test_2;
 
 
-var_dump($test);
+
+var_dump($test_3);
 
 // echo $test;
 
