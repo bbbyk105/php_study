f4bf309f42dce027901ae4f1f5f0c0f7c4986a93
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-07 11:43:56 +0900

    演算子

--- index.php
+++ index.php
@@ -1,73 +1,11 @@
 <?php
 
-$array_1 = ["あああ", 2, 3];
+$test_1 = 2;
+$test_2 = 3;
 
-$array_2 = [
-    ["赤", "青", "黄色"],
-    ["白", "紫", "黒"]
-];
-// echo $array[0];
-
-echo ('<pre>');
-var_dump($array_2);
-echo ('<pre>');
-
-// echo $array_2[1][1];
-
-$array_member = [
-    'name' => "ホンダ",
-    'height' => 170,
-    'hobby' => 'サッカー'
-];
-
-echo $array_member['hobby'];
-echo '<pre>';
-var_dump($array_member);
-echo '</pre>';
-
-$array_member_2 = [
-    'ホンダ' => [
-        'height' => 170,
-        'hobby' => "soccer"
-    ],
-    '香川' => [
-        'height' => 165,
-        'hobby' => 'soccer'
-    ]
-];
-
-echo $array_member_2['香川']['height'];
-
-echo '<pre>';
-var_dump($array_member_2);
-echo '</pre>';
+$test_3 = $test_1 % $test_2;
 
 
-$array_member_3 = [
-    '1組' => [
-        'ホンダ' => [
-            'height' => 170,
-            'hobby' => "soccer"
-        ],
-        '香川' => [
-            'height' => 165,
-            'hobby' => 'soccer'
-        ]
-    ],
-    '2組' => [
-        '長友' => [
-            'height' => 160,
-            'hobby' => 'soccer'
-        ],
-        '乾' => [
-            'height' => 168,
-            'hobby' => 'soccer'
-        ]
-    ]
 
-
-];
-
-echo $array_member_3['2組']['長友']['height'];
-
+echo $test_3;
 ?>
