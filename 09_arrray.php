44e0af0b8535c5b85b6e7b570845d8c143dcc62b
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-07 11:20:17 +0900

    連想配列

---
+++ 09_arrray.php
@@ -0,0 +1,17 @@
+<?php
+
+$array_1 =["ああああ",2,3];
+
+$array_2=[
+    ["あか","青", "黄色"],
+    ["白","紫","黒"]
+];
+// echo $array[0];
+
+echo ('<pre>');
+var_dump($array_2);
+echo ('<pre>');
+
+echo $array_2[1][1];
+
+?>
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-$array_1 =["ああああ",2,3];
+$array_1 =["あああ",2,3];
 
 $array_2=[
-    ["あか","青", "黄色"],
+    ["赤","青", "黄色"],
     ["白","紫","黒"]
 ];
 // echo $array[0];
@@ -12,6 +12,18 @@ echo ('<pre>');
 var_dump($array_2);
 echo ('<pre>');
 
-echo $array_2[1][1];
+// echo $array_2[1][1];
+
+$array_member = [
+    'name' => "ホンダ",
+    'height' => 170,
+    'hobby' => 'サッカー'
+];
+
+echo $array_member['hobby'];
+echo '<pre>';
+var_dump($array_member);
+echo '</pre>'
 
 ?>
+
