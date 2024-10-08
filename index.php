dd5082c147641ec3e359df6bfe5a280d8ca4b614
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-08 15:15:41 +0900

    forとwhile

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 
+//for while
 
-$members_2 = [
-    'ホンダ' => [
-        'height' => 170,
-        'hobby' => 'サッカー'
-    ],
-    '香川' => [ 
-        'height'  => 165,
-        'hobby' => 'サッカー'
-    ]
-];
+//continue, break
 
-// 多段階の配列
-foreach ($members_2 as $member_1) {
-    foreach($member_1 as $member => $value) {
-        echo $member . 'は' . $value . 'です';
-    };
+
+for ($i = 0; $i < 10; $i++) {
+    if ($i === 5) {
+        break;
+    }
+    echo $i;
 }
 
+echo '<br>';
+
+$j = 0;
+while($j < 5){
+    echo $j;
+    $j++;
+}
 ?>
