ff073e38eda4cbb9321b767174da23ccd8e595c7
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-11 19:35:32 +0900

    str

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <?php
 
-// 文字列の長さ
+// 指定文字列で分割
 
-// マルチバイト
-// 日本語 SJIS, UTF-8 3-6bite
-$text = 'あいうえお';
+$str_2 = '指定文字列で分解します';
 
-// echo strlen($text);
+echo "<pre>";
+var_dump(explode('、', $str_2));
 
-echo mb_strlen($text);
+// implode
 
+// 正規表現
+// 文字かどうか
+// 数字かどうか
+// 郵便番号
+// メールアドレス
 
-// 文字列の置換
+$str_3 = '特定の文字列が含まれるか確認する。';
 
-$str ='文字列を置換します';
+echo preg_match('/文字列/', $str_3);
 
-echo str_replace('置換','ちかん', $str);
+// 指定文字列から文字列を取得する。
 
-
+echo substr('abcde', 2);
+echo mb_substr('かきくけこ', 2)
 
 ?>
