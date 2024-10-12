a8c56345e2f431ae49d5cdd60acb10e1d5bf787d
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-12 16:34:09 +0900

    配列項目の追加

--- index.php
+++ index.php
@@ -1,27 +1,14 @@
 <?php
+// 配列についかする
+$array = ['りんご', 'みかん'];
 
-// 指定文字列で分割
+array_push($array, 'ブドウ','なし');
 
-$str_2 = '指定文字列で分解します';
+echo '<pre>';
+var_dump($array);
+echo '<pre>'
 
-echo "<pre>";
-var_dump(explode('、', $str_2));
 
-// implode
 
-// 正規表現
-// 文字かどうか
-// 数字かどうか
-// 郵便番号
-// メールアドレス
-
-$str_3 = '特定の文字列が含まれるか確認する。';
-
-echo preg_match('/文字列/', $str_3);
-
-// 指定文字列から文字列を取得する。
-
-echo substr('abcde', 2);
-echo mb_substr('かきくけこ', 2)
 
 ?>
