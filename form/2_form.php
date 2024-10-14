282cd56908ce5bfd4fbfc480d957bf1c1ac059f8
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-14 18:39:37 +0900

    お問い合わせフォームの基礎

---
+++ form/2_form.php
@@ -0,0 +1,34 @@
+<?php
+if (!empty($_POST)) {
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form method="POST" action="input.php">
+        氏名
+        <input type="text" name="your_name">
+        <br>
+        <input type="checkbox" name="sports[]" value="野球"> 野球
+        <input type="checkbox" name="sports[]" value="サッカー"> サッカー
+        <input type="checkbox" name="sports[]" value="バスケ"> バスケ
+
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
