454ac5ffd69b36885404da60befcfd67ef1d1eef
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-16 11:49:26 +0900

    XSS

--- form/input.php
+++ form/input.php
@@ -2,6 +2,8 @@
 
 <?php
 
+header('X-FRAME-OPTIONS:DENY');
+
 $pageFlag = 0;
 
 if (!empty($_POST['btn_confirm'])) {
@@ -32,10 +34,10 @@ function h($str)
     <?php if ($pageFlag === 0): ?>
         <form method="POST" action="input.php">
             氏名
-            <input type="text" name="your_name" value="<?php if(!empty($_POST["your_name"])){echo $_POST["your_name"];} ?>">
+            <input type="text" name="your_name" value="<?php if(!empty($_POST["your_name"])){echo h($_POST["your_name"]);} ?>">
             <br>
             メールアドレス
-            <input type="email" name="email" value="<?php if(!empty($_POST["email"])){echo $_POST["email"];} ?>">
+            <input type="email" name="email" value="<?php if(!empty($_POST["email"])){echo h($_POST["email"]);} ?>">
             <br>
             <input type="submit" name="btn_confirm" value="確認する">
         </form>
@@ -45,15 +47,15 @@ function h($str)
     <?php if ($pageFlag === 1): ?>
         <form method="POST" action="input.php">
             氏名
-            <?php echo $_POST["your_name"]; ?>
+            <?php echo h($_POST["your_name"]); ?>
             <br>
             メールアドレス
-            <?php echo $_POST["email"]; ?>
+            <?php echo h($_POST["email"]); ?>
             <br>
             <input type="submit" name="back" value="戻る">
             <input type="submit" name="btn_submit" value="送信する">
-            <input type="hidden" name="your_name" value="<?php echo $_POST["your_name"]; ?>">
-            <input type="hidden" name="email" value="<?php echo $_POST["email"]; ?>">
+            <input type="hidden" name="your_name" value="<?php echo h($_POST["your_name"]); ?>">
+            <input type="hidden" name="email" value="<?php echo h($_POST["email"]); ?>">
         </form>
     <?php endif; ?>
 
