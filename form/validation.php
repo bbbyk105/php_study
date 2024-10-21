d82512ce35bbb7d0c1ae7b53e8411cd140da6fda
Byakko Kondo <bbbyk105@yahoo.co.jp>
2024-10-21 11:59:16 +0900

    validationの追加

--- form/validation.php
+++ form/validation.php
@@ -5,8 +5,12 @@ function validation($request)
 
     $errors = [];
 
-    if (empty($request['your_name'])) {
-        $errors[] = '氏名は必須です';
+    if (empty($request['your_name']) || 20 < mb_strlen($request['your_name'])) {
+        $errors[] = '氏名は必須です。20文字以内で入力してください。';
+    }
+
+    if (empty($request['your_name']) || 200 < mb_strlen($request['your_name'])) {
+        $errors[] = 'お問い合わせ内容は必須です。200文字以内で入力してください。';
     }
 
     return $errors;
